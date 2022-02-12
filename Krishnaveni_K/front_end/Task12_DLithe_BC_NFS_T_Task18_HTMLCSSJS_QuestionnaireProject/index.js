f3fc95860596e83dc9cbbe922fb48be414c8cb42
“KrishnaveniKAPS”@gmail.com” <kkrishnaveni644@gmail.com>
2022-02-12 16:32:56 +0530

    firstcommit task

--- Krishnaveni_K/front_end/Task12_DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject/index.js
+++ Krishnaveni_K/front_end/Task12_DLithe_BC_NFS_T_Task18_HTMLCSSJS_QuestionnaireProject/index.js
@@ -1,29 +1,29 @@
 //Question bank
 var questionBank= [
     {
-        question : 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of',
-        option : ['Asia','Africa','Europe','Australia'],
-        answer : 'Africa'
+        question : 'The h1 element in the HTML defines',
+        option : ['Hyperlink','Headings','HyperText','Html-text'],
+        answer : 'Headings'
     },
     {
-        question : 'Garampani sanctuary is located at',
-        option : ['Junagarh, Gujarat','Diphu, Assam','Kohima, Nagaland','Gangtok, Sikkim'],
-        answer : 'Diphu, Assam'
+        question : 'For displaying a webpage within a webpage,HTML uses',
+        option : ['Classes','Iframes','Div element','Span Element'],
+        answer : 'Iframes'
     },
     {
-        question : 'For which of the following disciplines is Nobel Prize awarded?',
-        option : ['Physics and Chemistry','Physiology or Medicine','Literature, Peace and Economics','All of the above'],
-        answer : 'All of the above'
+        question : 'Which of the following selector matches a element based on its class attribute?',
+        option : ['The Type Selector','The Universal Selector','The Class Selector','All of the above'],
+        answer : 'The Class Selector'
     },
     {
-        question : 'Hitler party which came into power in 1933 is known as',
-        option : ['Labour Party','Nazi Party','Ku-Klux-Klan','Democratic Party'],
-        answer : 'Nazi Party'
+        question : 'If we want to use a nice green dotted border around an image, which css property are we going to use?',
+        option : ['border-line','border-style','border-decoration','border-color'],
+        answer : 'border-color'
     },
     {
-        question : 'First human heart transplant operation conducted by Dr. Christiaan Barnard on Louis Washkansky, was conducted in',
-        option : ['1967','1968','1958','1922'],
-        answer : '1967'
+        question : 'Which of the following type of variable is visible everywhere in your JavaScript code?',
+        option : ['Global variable','local variable','Both of the above','None of the above'],
+        answer : 'Global variable'
     }
 ]
 
