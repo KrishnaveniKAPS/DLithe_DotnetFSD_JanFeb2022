e7233523124cc280b0784b444470d35f2db95c88
“KrishnaveniKAPS”@gmail.com” <kkrishnaveni644@gmail.com>
2022-02-22 11:44:36 +0530

    Merge branch 'master' of https://github.com/KrishnaveniKAPS/DLithe_DotnetFSD_JanFeb2022

---
+++ Krishnaveni_K/front_end/CSharp/Task5/Passout.cs
@@ -0,0 +1,17 @@
+﻿class Passout
+    {
+        public static string GetNextNameByOut(out int id)
+        {
+            id = 1;
+            string returnText = "Next-" + id.ToString();
+            return returnText;
+        }
+        static void  main(string[] args)
+        {
+            int i = 0;
+            Console.WriteLine("Previous value of integer i:" + i.ToString());
+            string test = GetNextNameByOut(out i);
+            Console.WriteLine("Current value of integer i:" + i.ToString());
+        }
+    }
+
