546c3c100b6a35c2521f4b2550a7e3b184d47c90
“KrishnaveniKAPS”@gmail.com” <kkrishnaveni644@gmail.com>
2022-02-23 12:18:29 +0530

    Task5firstcommit

---
+++ Krishnaveni_K/front_end/CSharp/Task5/Abstract.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Abstract
+    {
+        // C# program to calculate the area
+// of a Square using abstract class
+// and abstract method
+ 
+// declare class 'AreaClass'
+// as abstract
+abstract class AreaClass
+    {
+        // declare method
+        // 'Area' as abstract
+        abstract public int Area();
+    }
+
+    // class 'AreaClass' inherit
+    // in child class 'Square'
+    class Square : AreaClass
+    {
+        int side = 0;
+
+        // constructor
+        public Square(int n)
+        {
+            side = n;
+        }
+
+        // the abstract method
+        // 'Area' is overridden here
+        public override int Area()
+        {
+            return side * side;
+        }
+    }
+
+    class gfg
+    {
+
+        // Main Method
+        public static void main()
+        {
+            Square s = new Square(6);
+            Console.WriteLine("Area  = " + s.Area());
+        }
+    }
+}
+}
