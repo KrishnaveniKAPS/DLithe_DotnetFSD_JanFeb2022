8bda19c371cc2eb8ceff1d809bb861a863e2e77c
“KrishnaveniKAPS”@gmail.com” <kkrishnaveni644@gmail.com>
2022-02-26 09:28:45 +0530

    commit

---
+++ Krishnaveni_K/back_end/SQL/SQLQuery1.sql
@@ -0,0 +1,14 @@
+--CREATE DATABASE
+create database DLithe
+USE DLithe
+--CREATE TABLE
+create table Trainees
+(
+ID int NOT NULL,
+Name varchar(20) NOT NULL
+);
+--INSERT RECORDS INTO THE TABLE
+insert into Trainees values(1,'jhon');
+insert into Trainees values(2,'jhonk');
+--SELECT 
+select * from Trainees
